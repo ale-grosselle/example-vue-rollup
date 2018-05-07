69a6bec887f4d90e845e3d077524514c34853a41
Alessandro Grosselle <alessandro.grosselle@thron.com>
2018-05-07 21:11:03 +0200

    update files

--- src/vanilla/genericComponent/button/index.js
+++ src/vanilla/genericComponent/button/index.js
@@ -1,15 +1,7 @@
 //@flow
-import indexScss from "vanilla/genericComponent/button/index.scss";
-import styleInject from "style-inject";
 import IComponent from "vanilla/genericComponent/IComponent";
-
-let neverInject:boolean = true;
 export default class Button extends IComponent {
 	constructor() {
 		super();
-		if (neverInject) {
-			styleInject(indexScss);
-			neverInject = false;
-		}
 	}
 }
